d20668a1e983aea05bcab95d7886384594ce2262
ReStranger <strengerplayr@gmail.com>
2024-11-23 21:11:13 +0300

    Use eslint to project
    Signed-off-by: ReStranger <strengerplayr@gmail.com>

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,16 +1,16 @@
 import "../../styles/components/header.scss";
 export const Header = () => {
-  return (
-    <header>
-      <nav>
-        <ul>
-          <li>Home</li>
-          <li>About me</li>
-          <li>Project</li>
-          <li>Contacts</li>
-        </ul>
-      </nav>
-    </header>
-  );
+    return (
+        <header>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>About me</li>
+                    <li>Project</li>
+                    <li>Contacts</li>
+                </ul>
+            </nav>
+        </header>
+    );
 };
 export default Header;
